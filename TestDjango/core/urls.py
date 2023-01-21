ce7a9eaeb8e76abfdb578040a5c07395c9dd3069
jcosses <120616033+jcosses@users.noreply.github.com>
2023-01-20 22:19:32 -0300

    Add files via upload
    TestDjango 20-01

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home, test
+from .views import home, datos
 
 urlpatterns = [
-    path('',home,name="home"),
-    path('datos',test,name="datos"),
+    path('',home,name="home.html"),
+    path('datos',datos,name="datos.html"),
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, 'home.html')
 
-def test(request):
-    lista=["Lazaña","Charquican","Porotos Granados"]
-    hijo=Class_Persona("Fernando Rivera","23")
-    contexto = {"nombre":"Juan Carlos Osses","comidas":lista,"hijo":hijo}
-    return render(request, 'core/datos.html', contexto)
+def datos(request):
+    # contexto={"nombre":"JUAN CARLOS"}
+    hijo=Class_Persona("Martin Cabrera","23")
+    contexto={"nombre":"JUAN CARLOS","pariente":hijo}
+    return render(request, 'datos.html',contexto)
 
 class Class_Persona:
     def __init__(self, nombre, edad):
         self.nombre=nombre
         self.edad=edad
         super().__init__()
+        
